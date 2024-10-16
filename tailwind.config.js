dcbe011195a3babbfadd6400be29fdba696a709d
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-16 18:09:14 -0400

    chore(tailwind): add custom color palette and new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: [ "./src/**/*.{js,ts,jsx,tsx}", ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        textSoft: '#0277EE',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
