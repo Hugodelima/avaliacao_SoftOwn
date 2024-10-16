b7c5e004f6aa8a07a6b52d606462fc126ba02e06
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-16 18:24:57 -0400

    chore(header): add navbar  and add overlay image and add css responsive to overlay

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
+import logoWhite from '../assets/Logo_white.png'
+import first from '../assets/first.png'
+import '../styles/header.css'
 function Header() {
 
   return (
     <>
+      <div className='header'>
+
+        <div className='flex justify-between p-4 sm:p-10 md:p-16 items-center gap-4'>
+          <img src={logoWhite} alt='SoftOwn Solutions' className='z-10 h-14 sm:h-16 md:h-24 w-auto' />
+          <button className='text-textSoft z-10 bg-white h-10 rounded-md text-sm sm:p-2 sm:text-base md:text-xl font-b'>
+            Acessar o <span className='font-bold'>Sistema</span>
+          </button>
+        </div>
+      </div> 
     </>
   )
 }
