d4f899eb771d30a344e3d75b23d7ba2086a9cffa
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-17 17:49:43 -0400

    feat(tailwind): add a new pallet color to background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       colors:{
         textSoft: '#0277EE',
+        background: '#F8F9FA',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
