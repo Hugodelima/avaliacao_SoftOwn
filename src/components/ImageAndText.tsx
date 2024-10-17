95b68165cd28dbce90b1df7120391c1990584f7f
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-17 18:04:29 -0400

    feat(ImageAndText): created new component to show image and text in one component

--- src/components/ImageAndText.tsx
+++ src/components/ImageAndText.tsx
@@ -1,39 +1,51 @@
-import { useState } from "react";
-
 interface ImageAndTextProps {
   direction: string;
   imageComponent: string; 
   title: string;
   description: string;
   listMarcation: string[]; 
+  background: boolean;
 }
 
-function ImageAndText({direction, imageComponent,title,description,listMarcation}: ImageAndTextProps){
+function ImageAndText({direction, imageComponent,title,description,listMarcation, background}: ImageAndTextProps){
 
-  const [side, setSide] = useState('r')
-  
-  switch (direction) {
-    case 'r':
-      break;
-    case 'l':
-      break;
-    default:
-      break;
-  }
   return (
 
     <>
-      <div>
-        <img src={imageComponent}/>
-        <h2>{title}</h2>
-        <h3>{description}</h3>
-        { 
-        listMarcation.map ( ( string:string ) => {
-            return(
-                <li>{string}</li>
-            )
-        }  )}
-      </div> 
+      <div className={ background ? "flex flex-wrap xl:flex-nowrap p-40 bg-background gap-10" : 'flex flex-wrap xl:flex-nowrap p-40 gap-10'}>
+        {direction === 'L' ? (
+
+          <div>
+            <img src={imageComponent} className="" />
+          </div>
+        ) : null}
+
+        <div className="flex flex-col justify-center mb-32">
+          <h2 className="text-textSoft font-bold text-xl mb-4">{title}</h2>
+          <h3>{description}</h3>
+          { 
+          listMarcation.map((string, index) => {
+            const position = string.indexOf(':') + 1;
+            return (
+              <div className="flex gap-2" key={index}>
+                  <i className="bi bi-check-circle-fill text-textSoft"></i>
+                  <li className="mb-2">
+                      <span className="text-textSoft font-semibold">{string.substring(0, position)}</span>
+                      {string.substring(position, string.length)}
+                  </li>
+              </div>
+            );
+          })}
+        </div>
+
+        {direction === 'R' ? (
+            <div>
+                <img src={imageComponent} className="" />
+            </div>
+        ) : null}
+  </div>
+
+        
     </>
   )
 }
