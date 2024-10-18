65e4697518ac2e6db0f05bc2a37b1377d2fd8ea8
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-18 00:05:56 -0400

    fix(app): adjust of image responsive to align the center

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ function App(){
 
             <ImageAndText direction={'L'} imageComponent={fifth} title={'Agenda e prontuário de seus pacientes.'} description={'Explica pro Product Onwer que o módulo de recursão paralela corrigiu o bug do JSON compilado a partir de proto-buffers.'} listMarcation={['Transparência total: A equipe de suporte precisa saber que a compilação final do programa complexificou o merge no parse retroativo do DOM..', 'Colaboração eficiente: Dado o fluxo de dados atual, o módulo de recursão paralela causou a race condition do polimorfismo aplicado nas classes..']} background={true} />
 
-            <div className={`flex flex-wrap xl:flex-nowrap gap-10 bg-textSoft`}>
-                <div className="flex flex-col justify-center mb-32 p-16">
+            <div className={`flex flex-wrap xl:flex-nowrap gap-0 bg-textSoft`}>
+                <div className="flex flex-col justify-center mb-12 p-16">
                     <h2 className="text-white font-bold text-xl mb-4">Elimine todos papéis do seu escritório.</h2>
                 
                     <div className="flex gap-2 flex-col">
@@ -59,10 +59,14 @@ function App(){
                 </div>
 
                
-                <div className="relative flex justify-center items-center">
-                    <img src={sixty_decoration} className="w-auto h-auto" />
-                    <img src={ilustration} className="absolute left-0" />
+                <div className="relative flex justify-center items-center pl-4">
+                    <img src={sixty_decoration} className="m-auto h-auto" />
+                    <img src={ilustration} className="absolute left-0 md:left-[30%] xl:left-0 pl-4 lg:w-[40rem] h-auto" />
                 </div>
+                {/*<div className="relative flex justify-center items-center ">
+                    <img src={sixty_decoration} className=" m-auto h-auto" />
+                    <img src={ilustration} className="absolute left-0" />
+                </div> */}
 
 
                
