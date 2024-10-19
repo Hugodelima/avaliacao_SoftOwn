252449987073f250a4274651ecd5c60644f236bf
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-18 21:52:43 -0400

    feat:; add a new pallet of color green

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         textSoft: '#0277EE',
         background: '#F8F9FA',
         textGray: '#6B7280',
+        textGreen: '#26B99A',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
