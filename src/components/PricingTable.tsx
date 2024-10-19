481f3019cf4ad45b193648eed3123e458154580f
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-18 21:59:15 -0400

    fix: manking better responsive to md size screen

--- src/components/PricingTable.tsx
+++ src/components/PricingTable.tsx
@@ -8,24 +8,29 @@ interface PricingTableProps {
 function PricingTable( {title, description, price, listAdvantage }: PricingTableProps){
   return (
 
-    <div className="border-solid border-2 rounded-md border-gray-400 border-opacity-10 p-4">
+    <div className="border-solid border-2 rounded-md border-gray-400 shadow-md border-opacity-10 p-4">
       <h3 className='text-textSoft text-center font-bold text-xl mb-1'>{title}</h3>
       <p className="font-light text-center w-[80%] m-auto">{description}</p>
       <div className="flex justify-center mt-4 mb-8 items-end">
         <p className="text-textSoft text-5xl font-black">R${price}</p>
         <p className="font-light">/mês</p>
       </div>
-      <div>
+      <div className="flex flex-col gap-4">
         {
         listAdvantage.map((string, index) => {
           return(
             <div className="flex gap-2" key={index}>
-              <i className="bi bi-check-circle-fill text-textSoft"/>
+              <i className="bi bi-check-circle-fill text-textGreen"/>
               <li className=" text-textGray">{string}<span className="font-bold text-textGray">fggfdgf</span></li>
             </div>
 
           );
         })}
+      </div>
+      <div className="mt-8">
+        <button className='text-white bg-textSoft rounded-md p-2 font-semibold border-solid border-2 border-white w-[100%]'>
+          Selecionar este plano
+        </button>
       </div>
     </div>
   )
